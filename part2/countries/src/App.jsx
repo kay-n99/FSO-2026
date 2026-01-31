297c4e60a5b36804cf16ec292e4399e7013e4128
kay_yn99 <kayanadonis@gmail.com>
2026-01-31 11:26:39 +0700

    feat: exercise 2.18 solution

---
+++ part2/countries/src/App.jsx
@@ -0,0 +1,75 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+const App = () => {
+  const [countries, setCountries] = useState(null);
+  const [searchC, setSearchC] = useState("");
+
+  useEffect(() => {
+    console.log("fetching datas...");
+    axios
+      .get(`https://studies.cs.helsinki.fi/restcountries/api/all`)
+      .then((response) => {
+        console.log(response.data);
+        setCountries(response.data);
+      });
+  }, [countries]);
+
+  const searched =
+    searchC.length == 0 || countries == null ? (
+      <></>
+    ) : (
+      countries.filter((c) =>
+        c.name.common.toLowerCase().includes(searchC.toLowerCase()),
+      )
+    );
+
+  const handleSearchChange = (event) => {
+    setSearchC(event.target.value);
+  };
+
+  return (
+    <>
+      find countries <input onChange={handleSearchChange} value={searchC} />
+      <div>
+        {searchC.length > 0 ? (
+          countries ? (
+            searched.length > 10 ? (
+              <span>Too many matches, specify another filter</span>
+            ) : searched.length == 1 ? (
+              searched.map((c) => {
+                return (
+                  <div>
+                    <h2>{c.name.common}</h2>
+                    <span>Capital {c.capital}</span>
+                    <br />
+                    <span>Area {c.area}</span>
+                    <h2>Languages</h2>
+                    <ul>
+                      {Object.keys(c.languages).map((key, index) => (
+                        <li key={index}>{c.languages[key]}</li>
+                      ))}
+                    </ul>
+                    <img src={c.flags.png}></img>
+                  </div>
+                );
+              })
+            ) : (
+              searched.map((c) => (
+                <>
+                  <span key={c.cca3}>{c.name.common}</span>
+                  <br />
+                </>
+              ))
+            )
+          ) : (
+            <span>fetching datas...</span>
+          )
+        ) : (
+          <span>Search for a country</span>
+        )}
+      </div>
+    </>
+  );
+};
+
+export default App;
