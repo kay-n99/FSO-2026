fd1c1ae3002fe3d8f79220dea7513e64bba9b7c8
kay_yn99 <kayanadonis@gmail.com>
2026-03-06 17:47:49 +0700

    feat: exercise ~3.8

--- part3/phonebook/src/index.js
+++ part3/phonebook/src/index.js
@@ -1,12 +1,15 @@
 //  const http = require('http') 
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
+
 app.use(express.json())
+
+morgan.token('body', (req) => JSON.stringify(req.body))
+app.use(morgan(' :method :url :status :res[content-length] - :response-time ms :body'))
+
+
  
-//  const app = http.createServer((request, response) => {
-//     response.writeHead(200, { 'Content-Type': 'application/json'})
-//     response.end(JSON.stringify(persons))
-// })
 let persons = [
     { 
       "id": "1",
