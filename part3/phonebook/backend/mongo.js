3a270d0581187224cfd90b231373adccfb5214c6
kay_yn99 <kayanadonis@gmail.com>
2026-03-08 09:57:05 +0700

    feat: exercise solution 3.12

---
+++ part3/phonebook/backend/mongo.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose')
+
+const password = process.argv[2]
+
+const url = `mongodb+srv://admin:${password}@cluster0.yl1unwx.mongodb.net/?appName=Cluster0`
+
+mongoose.set('strictQuery', false)
+
+mongoose.connect(url, { family: 4})
+
+const personSchema = new mongoose.Schema({
+    name: String,
+    number: String,
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+if(process.argv.length == 3){
+   Person.find().then(result => {
+    result.forEach(p=> {
+        console.log(p)
+    })
+    mongoose.connection.close()
+   })
+}else if(process.argv.length == 5){
+    const person = new Person({
+        name: process.argv[3],
+        number: process.argv[4],
+    })
+
+    person.save().then(result => {
+        console.log('phone saved!')
+        mongoose.connection.close()
+    })
+}else{
+    console.log("please provide correct arguments")
+}
