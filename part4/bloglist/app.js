bcd5560fdfa9900ecd6f7d3990e11e9fd9f6e660
kay_yn99 <kayanadonis@gmail.com>
2026-03-19 07:12:05 +0700

    feat:exercise 4.18 solution

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose')
 const config = require('./utils/config')
 const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 const middleware = require('./utils/middleware')
 
 const app = express()
@@ -13,6 +14,7 @@ mongoose.connect(config.MONGODB_URI, { family: 4 })
 app.use(express.json())
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 
 app.use(middleware.errorHandler)
 
