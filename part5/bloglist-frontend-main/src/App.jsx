f5304f01a0d27e31a479559e6d966828dc22daf3
kay_yn99 <kayanadonis@gmail.com>
2026-03-21 13:50:03 +0700

    feat:exercise 5.2

--- part5/bloglist-frontend-main/src/App.jsx
+++ part5/bloglist-frontend-main/src/App.jsx
@@ -15,11 +15,26 @@ const App = () => {
     blogService.getAll().then((blogs) => setBlogs(blogs));
   }, []);
 
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedBlogAppUser')
+    if(loggedUserJSON){
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+      blogService.setToken(user.token)
+    }
+  },[])
+
   const handleLogin = async (event) => {
     event.preventDefault();
 
     try {
       const user = await loginService.login({ username, password });
+
+      window.localStorage.setItem(
+        'loggedBlogAppUser', JSON.stringify(user)
+      )
+      blogService.setToken(user.token)
+
       setUser(user);
       setUsername("");
       setPassword("");
@@ -30,6 +45,11 @@ const App = () => {
       }, 5000);
     }
   };
+
+  const handleLogout = () => {
+    window.localStorage.removeItem('loggedBlogappUser')
+    setUser(null)
+  }
 
   if (user === null) {
     return (
@@ -65,7 +85,7 @@ const App = () => {
   return (
     <div>
       <h2>{errorMessage}</h2>
-      <h2>{user.username} logged in</h2>
+      <h2>{user.username} logged in</h2> <button onClick={handleLogout}>logout</button>
       <h2>blogs</h2>
       {blogs.map((blog) => (
         <Blog key={blog.id} blog={blog} />
