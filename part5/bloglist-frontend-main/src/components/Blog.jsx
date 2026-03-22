1d93c75b6479a95f2a0eb56b183ea345683c4cb9
kay_yn99 <kayanadonis@gmail.com>
2026-03-22 19:27:42 +0700

    feat:exercise 5.7

--- part5/bloglist-frontend-main/src/components/Blog.jsx
+++ part5/bloglist-frontend-main/src/components/Blog.jsx
@@ -1,7 +1,35 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react";
 
-export default Blog
+const Blog = ({ blog }) => {
+  const [visible, setVisible] = useState(false);
+
+  const hideWhenVisible = { display: visible ? "none" : "" };
+  const showWhenVisible = {display: visible ? "" : "none"} ;
+  const label = visible ? "hide" : "view";
+
+  const toggleVisibility = () => {
+    setVisible(!visible);
+  };
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  return (
+    <div style={blogStyle}>
+      {blog.title} {blog.author}
+      <button onClick={toggleVisibility}>{label}</button>
+      <div style={showWhenVisible}>
+        <span>{blog.url}</span><br/>
+        <span>likes: {blog.likes}<button>like</button></span><br/>
+        <span>{blog.user.name}</span><br/>
+      </div>
+    </div>
+  );
+};
+
+export default Blog;
