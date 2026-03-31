e643d4d941f1e32cc2b7e8cabbbb74b817f892a4
kay <kayanadonis@gmail.com>
2026-03-31 14:03:55 +0700

    feat:exercise 6.4 solution

--- part6/redux-anecdotes-main/src/App.jsx
+++ part6/redux-anecdotes-main/src/App.jsx
@@ -1,5 +1,5 @@
 import { useSelector, useDispatch } from 'react-redux'
-import { addVote } from './reducers/anecdoteReducer'
+import { addVote, createAnecdote } from './reducers/anecdoteReducer'
 
 const App = () => {
   const anecdotes = useSelector(state => state)
@@ -8,6 +8,14 @@ const App = () => {
   const vote = id => {
     console.log('vote', id)
     dispatch(addVote(id))
+  }
+
+  const addAnecdote = e => {
+    e.preventDefault()
+    const content = e.target.anecdote.value
+    console.log('new anecdote', content)
+    dispatch(createAnecdote(content))
+    e.target.anecdote.value = ''
   }
 
   return (
@@ -23,11 +31,11 @@ const App = () => {
         </div>
       ))}
       <h2>create new</h2>
-      <form>
+      <form onSubmit={addAnecdote}>
         <div>
-          <input />
+          <input name='anecdote'/>
         </div>
-        <button>create</button>
+        <button type='submit'>create</button>
       </form>
     </div>
   )
--- part6/redux-anecdotes-main/src/reducers/anecdoteReducer.js
+++ part6/redux-anecdotes-main/src/reducers/anecdoteReducer.js
@@ -33,11 +33,10 @@ const reducer = (state = initialState, action) => {
       return state.map(anecdote => 
         anecdote.id !== id ? anecdote : changedAnecdote
       )
-      
+    case 'NEW_ANECDOTE':
+      return [...state, action.payload]      
   }
   
-
-
   return state
 }
 
@@ -45,6 +44,17 @@ export const addVote = id => {
   return {
     type: 'VOTE',
     payload: { id }
+  }
+}
+
+export const createAnecdote = content => {
+  return {
+    type: 'NEW_ANECDOTE',
+    payload: {
+      content: content,
+      id: getId(),
+      votes: 0
+    }
   }
 }
 
