d3e2223f7eddc8891d6ff1e66040c3fb9a537b52
kay <kayanadonis@gmail.com>
2026-04-06 04:57:21 +0700

    feat:exercise 7.4 solution

---
+++ part7/country-hook-main/src/hooks/index.js
@@ -0,0 +1,46 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+
+
+export const useField = (type) => {
+  const [value, setValue] = useState('')
+
+  const onChange = (event) => {
+    setValue(event.target.value)
+  }
+
+  return {
+    type,
+    value,
+    onChange
+  }
+}
+
+
+export const useCountry = (name) => {
+  const [country, setCountry] = useState(null)
+
+  useEffect(() => {
+    if(!name){
+      setCountry(null)
+      return
+    }
+
+    const fetchCountry = async () => {
+      try{
+        const response = await axios.get(
+          `https://studies.cs.helsinki.fi/restcountries/api/name/${name}`
+        )
+        setCountry({
+          data: response.data,
+          found: true
+        })
+      } catch(error){
+        setCountry({found: false})
+      }
+    }
+    fetchCountry()
+  }, [name])
+
+  return country
+}
