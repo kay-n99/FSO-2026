33ff157765e1af7d0017e3fa3a1b696cc07c48a7
kay <kayanadonis@gmail.com>
2026-04-06 03:54:34 +0700

    feat:exercise 7.1 solution

---
+++ part7/anecdotes-main/src/components/AnecdoteForm.jsx
@@ -0,0 +1,43 @@
+import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query'
+import { createAnecdote } from '../requests' 
+import { useContext } from 'react'
+import NotificationContext from  '../NotificationContext'
+
+const AnecdoteForm = () => {
+  const queryClient = useQueryClient()
+  const { setNotification  } = useContext(NotificationContext)
+
+  const newAnecdoteMutation = useMutation({
+    mutationFn: createAnecdote,
+    onSuccess: (newAnecdote) => {
+      // queryClient.invalidateQueries({ queryKey: ['anecdotes']})
+      const anecdotes = queryClient.getQueryData(['anecdotes'])
+      queryClient.setQueryData(['anecdotes'], anecdotes.concat(newAnecdote))
+    },
+    onError: (error) => {
+      const message = error.message
+      console.log(message)
+      setNotification(message, 5)
+    }
+  })
+
+  const onCreate = (event) => {
+    event.preventDefault()
+    const content = event.target.anecdote.value
+    event.target.anecdote.value = ''
+    newAnecdoteMutation.mutate({content, votes: 0})
+    setNotification(`you created '${content}'`, 5)
+  }
+
+  return (
+    <div>
+      <h3>create new</h3>
+      <form onSubmit={onCreate}>
+        <input name="anecdote" />
+        <button type="submit">create</button>
+      </form>
+    </div>
+  )
+}
+
+export default AnecdoteForm
