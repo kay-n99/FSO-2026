3a32642fb31eb78ae579f29116f39605bbc4c95c
kay <kayanadonis@gmail.com>
2026-04-09 07:03:16 +0700

    feat:exercise 8.9 solution

--- part8/library-frontend-main/src/components/Books.jsx
+++ part8/library-frontend-main/src/components/Books.jsx
@@ -1,9 +1,29 @@
+import { gql } from '@apollo/client'
+import { useQuery } from '@apollo/client/react'
+
+const ALL_BOOKS = gql`
+  query{
+    allBooks{
+      title
+      published
+      author
+      id
+    }
+  }
+`
+
 const Books = (props) => {
+  const books = useQuery(ALL_BOOKS)
+  
   if (!props.show) {
     return null
   }
 
-  const books = []
+  
+
+  if(books.loading){
+    return <div>loading...</div>
+  }
 
   return (
     <div>
@@ -16,7 +36,7 @@ const Books = (props) => {
             <th>author</th>
             <th>published</th>
           </tr>
-          {books.map((a) => (
+          {books.data.allBooks.map((a) => (
             <tr key={a.id}>
               <td>{a.title}</td>
               <td>{a.author}</td>
